746cfc5e127fd71b3a461d61749306a5380eea7a
Lee-Hwa-Jun <swag0090@naver.com>
2025-02-24 23:09:41 +0900

    방 생성/입장 통합

--- config/asgi.py
+++ config/asgi.py
@@ -3,7 +3,7 @@ from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from django.urls import re_path
-from config.consumers import GameConsumer, LobbyConsumer  # config/consumers.py에서 가져옴
+from config.consumers import GameConsumer  # config/consumers.py에서 가져옴
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.dev')
 
@@ -12,7 +12,6 @@ application = ProtocolTypeRouter({
     "websocket": AuthMiddlewareStack(
         URLRouter([
             re_path(r"ws/game/(?P<game>\w+)/(?P<room>\w+)$", GameConsumer.as_asgi()),
-            re_path(r"ws/game/omok/lobby$", LobbyConsumer.as_asgi()),
         ])
     ),
 })
